70782d4e0df776fcaf1ced567a9445360ca41f81
terrylashley <terry.l.lashley.pro@gmail.com>
2020-03-04 23:45:39 -0500

    update

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,50 @@
+#import dependencies
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+from splinter import Browser
+import requests
+
+
+#Set up the browser
+def browser_setup():
+    
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    
+#Make an empty dictionary to import to mongodb
+mars_data = {}
+
+#Nasa Mars News
+def scrape_mars_news():
+    try:
+        
+        #initiate the browser
+        browser = browser_setup()
+        
+        #Visit the Nasa Web Page with Splinter
+        url = 'https://mars.nasa.gov/news/'
+        browser.visit(url)
+        
+        #Create a HTML object to parse with Beautiful Soup
+        html = browser.html
+
+        #Parse HTML Object with Beautiful Soup
+        soup = bs(html, 'html.parser')
+        
+        #Retrieve the latest News Title and News Paragraph data
+        #Inspect web page for root path to information use soup.find to pull the data
+
+        news_title = soup.find('div', class_ ='list_text').find('a').text
+
+        news_paragraph = soup.find('div', class_='article_teaser_body').text
+        
+        #Place the News data into a dictionary
+        mars_data['news_title'] = news_title
+        mars_data['news_paragraph'] = news_paragraph
+        
+        return mars_data
+    
+    finally:
+        
+        browser.quit()
+        
