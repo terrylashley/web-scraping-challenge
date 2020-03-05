9c9532a692c340d14ff579f966ef89bf9e56eeea
terrylashley <terry.l.lashley.pro@gmail.com>
2020-03-05 09:17:21 -0500

    update

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -48,3 +48,42 @@ def scrape_mars_news():
         
         browser.quit()
         
+#JPL Mars Space Images
+def scrape_images():
+    try:
+        
+        #initiate the browser
+        browser = browser_setup()
+        
+        #Use splinter to find the url for the current featured image
+        mars_image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(mars_image_url)
+        
+        #create an HTML object to parse with Beautiful Soup
+        html_image = browser.html
+
+        #parse HTML with Beautiful Soup
+        soup = bs(html_image, 'html.parser')
+        
+        #grab the background image url
+        featured_image_url = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
+
+        #connect the main url for the website to the background image url
+        #base url
+        main_url = 'https://www.jpl.nasa.gov'
+
+        #join base url and background url
+        featured_image_url = main_url + featured_image_url
+        
+        #Full link for image
+        featured_image_url
+        
+        #add to the dictionary
+        mars_data['featured_image_url'] = featured_image_url
+        
+        return mars_data
+    finally:
+        
+        browser.quit()
+        
+        
